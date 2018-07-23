e889660b2b30b4bb0edad58f48cb602075d876c2
Nguyễn Cảnh Việt <gin.canhviet@gmail.com>
2018-07-23 23:25:00 +0700

    Add DB project

---
+++ src/GBlog.Database/CreateTable.sql
@@ -0,0 +1,77 @@
+﻿USE [GBlog]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Category](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](128) NOT NULL,
+	[ParentCategoryId] [int] NULL,
+	[ImagedURL] [nvarchar](400) NULL,
+	[PageSize] [int] NULL,
+	[ShowOnHomePage] [bit] DEFAULT 1,
+	[IncludeInTopMenu] [bit] DEFAULT 1,
+	[Published] [bit] NULL,
+	[CreatedDate] [datetime] NULL,
+	[CreatedBy] [nvarchar](128) NULL,
+	[UpdatedDate] [datetime] NULL,
+	[UpdatedBy] [nvarchar](128) NULL
+ CONSTRAINT [PK_dbo.Category] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Post](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UrlSlug] [nvarchar](512) NOT NULL,
+	[Title] [nvarchar](256) NOT NULL,
+	[Content] [ntext] NULL,
+	[Description] [ntext] NOT NULL,
+	[ImageUrl] [nvarchar](256) NULL,
+	[CreatedDate] [datetime] NULL,
+	[CreatedBy] [int] NULL,
+	[UpdatedDate] [datetime] NULL,
+	[UpdatedBy] [int] NULL,	
+	[Published] [bit] NULL,
+	[CategoryId] [int] NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [NameIndex] ON [dbo].[Category]
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+SET ANSI_PADDING ON
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UrlSlugIndex] ON [dbo].[Post]
+(
+	[UrlSlug] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
+ALTER TABLE [dbo].[Category]  WITH CHECK ADD  CONSTRAINT [FK_Category_Category] FOREIGN KEY([ParentCategoryId])
+REFERENCES [dbo].[Category] ([Id])
+GO
+
+ALTER TABLE [dbo].[Post]  WITH CHECK ADD  CONSTRAINT [FK_Post_Category] FOREIGN KEY([CategoryId])
+REFERENCES [dbo].[Category] ([Id])
+
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
